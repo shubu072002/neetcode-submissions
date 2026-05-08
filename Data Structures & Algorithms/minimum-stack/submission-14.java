6efd98d279d0c8b0c4e0549246a1163d45d73da2
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-05-08 23:47:36 +0530

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+    }
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || minStack.peek()>=val){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int top =   stack.pop();
+        if(top==minStack.peek()){
+            minStack.pop();
+        }
+      
+    }
+    
+    public int top() {
+     return stack.peek();
+    }
+    
+    public int getMin() {
+      return  minStack.peek();
+    }
+}
