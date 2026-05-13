2142ad294008b5fd300c53afa29933413fee7dde
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-05-13 17:14:52 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums,res,new ArrayList<>());
+        return res;
+    }
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> temp){
+        if(temp.size()==nums.length){
+            res.add(new ArrayList<>(temp));
+        }
+        for(int i=0;i<nums.length;i++){
+            if(temp.contains(nums[i])){
+                continue;
+            }
+            temp.add(nums[i]);
+            helper(nums, res, temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
