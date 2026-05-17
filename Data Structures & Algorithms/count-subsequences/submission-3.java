b086a57b64788d27b5bb737d381f5b4f7415dabc
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-05-17 15:30:04 +0530

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+       if(s.length()<t.length()) {
+        return 0;
+       }
+       int[][] dp = new int[s.length()+1][t.length()+1];
+       for(int i=0;i<dp.length;i++){
+        for(int j=0;j<dp[0].length;j++){
+            dp[i][j]=-1;
+        }
+       }
+       return helper(s,t,0,0,dp);
+    }
+    public int helper(String s, String t, int i, int j, int[][] dp){
+        if(j>=t.length()){
+            return 1;
+        }
+        if(i>=s.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int x =0;
+        if(s.charAt(i)==t.charAt(j)){
+             x = helper(s,t,i+1,j+1,dp);
+        }
+        int y = helper(s,t,i+1,j,dp);
+        return dp[i][j] = x + y;
+    }
+}
