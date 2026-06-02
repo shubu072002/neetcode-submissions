c5e9bc7897c5bbd303096918efb189db2e7d6d33
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-06-02 17:28:45 +0530

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                  if(board[i][j]=='.'){
+                    continue;
+                }
+                int num = board[i][j];
+                String row = num + "in row" + i;
+                String col = num + "in col" + j;
+                String box = num + "in box" + i/3+"-"+j/3;
+
+                if(!set.add(row) || !set.add(col) || !set.add(box)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
