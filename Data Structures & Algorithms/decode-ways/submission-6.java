4788aebd1d1a397d4f5c0b9ac2c08f0c4af7644a
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-07-21 23:36:17 +0530

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+       int n = s.length();
+       int[] dp = new int[n+1];
+       dp[0]=1;
+       dp[1]=s.charAt(0)=='0'?0:1;
+       for(int i=2;i<=n;i++){
+        int singledigit = Integer.parseInt(s.substring(i-1,i));
+        int doubledigit = Integer.parseInt(s.substring(i-2,i));
+        if(singledigit>=1){
+            dp[i]+=dp[i-1];
+        }
+        if(doubledigit<=26 && doubledigit>9){
+            dp[i]+=dp[i-2];
+        }
+       }
+       return dp[n];
+    }
+}
