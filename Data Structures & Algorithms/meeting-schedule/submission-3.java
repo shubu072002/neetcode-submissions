85225c9493512981193d714b5c101f3dcc33d641
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-08-11 22:49:44 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        int n = intervals.size();
+        if(n<1){
+            return true;
+        }
+       Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+        Interval newInterval=intervals.get(0);
+        for(int i=1;i<n;i++){
+            if(newInterval.end>intervals.get(i).start){
+                return false;
+            }
+            newInterval.end = intervals.get(i).end;
+        }
+        return true;
+    }
+}
