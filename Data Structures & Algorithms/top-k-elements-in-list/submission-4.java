db3c30da9ae4e9e956fbf97643460a41c658bdfa
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-08-30 23:22:01 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.java
@@ -0,0 +1,32 @@
+class Number implements Comparable<Number>{
+    int element;
+    int freq;
+    Number(int element, int freq){
+        this.element=element;
+        this.freq=freq;
+    }
+    public int compareTo(Number that){
+        return this.freq - that.freq;
+    }
+}
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Number> pq = new PriorityQueue<>();
+        for(Map.Entry<Integer, Integer> e: map.entrySet()){
+            pq.add(new Number(e.getKey(),e.getValue()));
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){
+            Number number=pq.poll();
+            res[i]=number.element;
+        }
+        return res;
+    }
+}
